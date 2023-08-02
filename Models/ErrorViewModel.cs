c77bf69d3ecb8e981a9304dd1e769e8786c88c02
Maks <maks.reznikov.1999@gmail.com>
2023-08-02 18:53:35 +0600

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lessonnginx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
